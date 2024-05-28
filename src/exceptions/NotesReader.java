971aef21a7ee5eb4e47d3d65bbeb37bf0181a023
Casua <Casua@Mert>
2024-05-28 12:07:15 +0200

    updated file to create a txt file with the notes

--- src/exceptions/NotesReader.java
+++ src/exceptions/NotesReader.java
@@ -1,7 +1,10 @@
 package exceptions;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -14,16 +17,29 @@ public class NotesReader {
 		System.out.println("L: 1/4");
 		System.out.println("K: C");
 		Scanner sc;
+		String output = "";
 		try{
 			sc = new Scanner(Paths.get("C:\\Users\\Casua\\eclipse-workspace\\mert_kisa_3HB\\src\\exceptions\\textnote.txt"));
 			while(sc.hasNextLine()) {
 				String str = sc.nextLine();
 				if(notes.matcher(str).matches()) {
+					output = output + str + " ";
 					System.out.print(str + " ");
 				}
 			}
 		}catch(IOException e) {
 			e.printStackTrace();
-		}	
+		}
+		try {
+			File file = new File ("C:\\Users\\Casua\\eclipse-workspace\\mert_kisa_3HB\\src\\exceptions\\textnote2.txt");
+		    PrintWriter noteWriter = new PrintWriter (file);
+		    noteWriter.println ("M: C");
+		    noteWriter.println("L: 1/4");
+		    noteWriter.println("K: C");
+		    noteWriter.print(output);
+		    noteWriter.close ();       
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
